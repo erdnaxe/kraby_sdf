961d91b64685133d8416e47228f2b1c86b4c67a6
Alexandre Iooss <erdnaxe@crans.org>
2020-06-02 15:19:10 +0200

    User input in simulation

--- simulation_demo.py
+++ simulation_demo.py
@@ -8,15 +8,32 @@ p.connect(p.GUI)
 
 p.setGravity(0, 0, -9.81)
 
+# Load a ground
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.loadSDF("plane_stadium.sdf")
 
+# Load robot, as SDFormat can contain multiple object, we select the first one
+# r will contain the robot object id
 p.setAdditionalSearchPath(os.getcwd())
-p.loadSDF("hexapod.sdf")
+r = p.loadSDF("hexapod.sdf")[0]
+
+# Get all motorized joints id and name (which are revolute joints)
+motorized_joints = [(j, p.getJointInfo(r, j)[1].decode())
+                    for j in range(p.getNumJoints(r))
+                    if p.getJointInfo(r, j)[2] == p.JOINT_REVOLUTE]
+
+# Create user debug interface
+motorized_joints_param = [(j, p.addUserDebugParameter(name, -0.5, 0.5, 0))
+                          for j, name in motorized_joints]
 
 while True:
+    # Read user input and simulate motor
+    for j, param in motorized_joints_param:
+        angle = p.readUserDebugParameter(param)
+        p.setJointMotorControl2(r, j, p.VELOCITY_CONTROL,
+                                targetVelocity=angle)
+
     p.stepSimulation()
-    time.sleep(1./240.)
 
 p.disconnect()
 
