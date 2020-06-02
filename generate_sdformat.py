594cb730f47344c14218bbb83e927e771ef8b8ab
Alexandre Iooss <erdnaxe@crans.org>
2020-06-02 18:10:06 +0200

    Remove spaces and factorize joints pose

--- generate_sdformat.py
+++ generate_sdformat.py
@@ -8,6 +8,7 @@ Compile SDFormat templates into SDFormat robot description
 loader = FileSystemLoader(searchpath="./src/")
 env = Environment(loader=loader)
 
+# Load and compile Jinja2 template
 template = env.get_template("kraby.sdf.j2")
 with open("hexapod.sdf", "w") as f:
     f.write(template.render())
