41fbcaeb0abae2a75983451b24fdbd4c96d4bcb7
Alexandre Iooss <erdnaxe@crans.org>
2020-06-02 17:37:07 +0200

    Force color in simulation

--- simulation_demo.py
+++ simulation_demo.py
@@ -4,8 +4,10 @@ import time
 import pybullet_data
 import os
 
+# Connect to Bullet GUI, can be DIRECT if no user inputs
 p.connect(p.GUI)
 
+# Newton's apple
 p.setGravity(0, 0, -9.81)
 
 # Load a ground
@@ -26,14 +28,21 @@ motorized_joints = [(j, p.getJointInfo(r, j)[1].decode())
 motorized_joints_param = [(j, p.addUserDebugParameter(name, -0.5, 0.5, 0))
                           for j, name in motorized_joints]
 
+# Add some colors
+# BUG: bullet does not load colors from material
+for i in range(37):
+    if i % 2:
+        p.changeVisualShape(r, i, rgbaColor=[1, 1, 0.7, 1])
+    else:
+        p.changeVisualShape(r, i, rgbaColor=[1, 0.7, 1, 1])
+
+# Simulation
 while True:
     # Read user input and simulate motor
     for j, param in motorized_joints_param:
-        angle = p.readUserDebugParameter(param)
-        p.setJointMotorControl2(r, j, p.VELOCITY_CONTROL,
-                                targetVelocity=angle)
+        v = p.readUserDebugParameter(param)
+        p.setJointMotorControl2(r, j, p.VELOCITY_CONTROL, targetVelocity=v)
 
     p.stepSimulation()
 
 p.disconnect()
-
